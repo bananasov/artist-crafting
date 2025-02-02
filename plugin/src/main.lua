67de8df4308dd82576d49fe54bb2ceef408b0783
sovnya <enp7s1@tutanota.com>
2025-02-02 14:42:00 +0100

    feat: Add basic plugin structure

--- plugin/src/main.lua
+++ plugin/src/main.lua
@@ -1,1 +1,7 @@
-print("Hello, World!")
+local logger = require("artist.lib.log")
+
+local log = logger.get_logger("artist-crafting")
+
+return function(context)
+    log("Starting up crafting plugin")
+end
